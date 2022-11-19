a6bfc414fa36f11e769a5d1499c62253ff0005e1
shashank lalit patil <shashankpatil125@gmail.com>
2022-11-19 16:22:33 +0000

    costimized ui

--- pages/donate/index.tsx
+++ pages/donate/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar'
 
 function donate() {
+  const [companyAddress, setCompanyAddress]=useState('');
+  const [personAddress, setPersonAddress]=useState('');
+  
+
   return (
     <>
       <Navbar />
-      <section className="text-gray-400 bg-gray-900 body-font h-full flex justify-center">
+      <section className="text-gray-400 bg-gray-900 body-font h-full lg:h-screen flex justify-center">
         <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
           
           <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
@@ -16,11 +20,11 @@ function donate() {
             <h2 className="text-white text-lg font-medium title-font mb-5">Helping Hands</h2>
             <div className="relative mb-4">
               <label htmlFor="company-id" className="leading-7 text-sm text-gray-400">Company ID</label>
-              <input type="text" name="company-id" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              <input value={companyAddress} onChange={(e)=>(setCompanyAddress(e.target.value))} type="text" name="company-id" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <div className="relative mb-4">
               <label htmlFor="person-id" className="leading-7 text-sm text-gray-400">Person ID</label>
-              <input type="text" name="person-id" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              <input value={personAddress} onChange={(e)=>(setPersonAddress(e.target.value))} type="text" name="person-id" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg hover:scale-90 duration-300">Donate</button>
             <p className="text-xs mt-3">You have the power to save a Life.</p>
